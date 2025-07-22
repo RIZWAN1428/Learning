7065caac6f82a5577a67711fd5408e7cec8868a8
Rizwan <mr659612@gmail.com>
2025-07-22 15:01:57 +0530

    Switch code updated

--- src/Switch.java
+++ src/Switch.java
@@ -8,6 +8,8 @@ public class Switch {
                     break;
             case 3: System.out.println("Wednesday");
                     break;
+            case 4: System.out.println("Thursday");
+                    break;
             default: System.out.println("Invalid Day !");
         }
     }
